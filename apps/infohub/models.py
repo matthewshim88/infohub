57b615360209fad2238ab4095ec06de193f24ef4
Hans Ravnaas <hravnaas@hotmail.com>
2016-09-26 21:53:25 -0700

    Added model and tests

--- apps/infohub/models.py
+++ apps/infohub/models.py
@@ -1,5 +1,82 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from ..login_reg.models import User
 
-# Create your models here.
+########## Info ##########
+
+class InfoSourceMgr(models.Manager):
+    # Adds a new source the user wants to use.
+    def add(self, data, user_id):
+        #TODO: Add check if source already exists.
+        MAX_NUM_SNIPPETS = 5
+        source = InfoSource.objects.create(
+            source_type = data['source_type'],
+            location = data['location'],
+            active = True, # Default to True until we allow user to pause
+            max_snippets = MAX_NUM_SNIPPETS,
+            highlight_text = data['highlight_text'],
+            user = User.objects.get(id = user_id)
+        )
+
+        Audit.objects.audit(user_id, "Added source")
+        return source
+
+    # Updates settings for an existing information source.
+    def update(self, data, user_id):
+        #TODO: Don't fail if source doesn't exist.
+        source = InfoSource.objects.get(id = data['source_id'])
+        source.source_type = data['source_type']
+        source.location = data['location']
+        source.active = True # Always set to True until we allow user to pause
+        #source.max_snippets = data['max_snippets'] # Not yet implemented
+        source.highlight_text = data['highlight_text']
+        source.save()
+
+        Audit.objects.audit(user_id, "Updated source")
+        return source
+
+    # Removes an information source.
+    # NOTE: This could instead be implemented with just setting a Deleted bit to 1.
+    def remove(self, data, user_id):
+        #TODO: Fail silently if source doesn't exist.
+        InfoSource.objects.get(id = data['source_id']).delete()
+
+        Audit.objects.audit(user_id, "Removed source")
+        return
+
+    def getActive(self, user_id):
+        return InfoSource.objects.filter(user_id = user_id).filter(active = True)
+
+# Handles manipulating information sources that are aggregated into the UI.
+class InfoSource(models.Model):
+    source_type = models.CharField(max_length=100) # Scrape or call API
+    location = models.CharField(max_length=500) # The location of the info (URL or API name)
+    active = models.BooleanField(default=True) # Whether this source is active
+    max_snippets = models.PositiveSmallIntegerField()
+    highlight_text = models.CharField(max_length=50) # Keyword/text to be highlighted on result page.
+    user = models.ForeignKey(User, related_name='sources')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = InfoSourceMgr()
+
+########## Audit ##########
+
+# Handles auditing of user/site activity.
+class AuditMgr(models.Manager):
+    def audit(self, user_id, action):
+        Audit.objects.create(
+            action = action,
+            user = User.objects.get(id = user_id)
+        )
+
+    def getAll(self, max_rows):
+        return Audit.objects.all().order_by('-created_at')[:max_rows]
+
+class Audit(models.Model):
+    action = models.CharField(max_length=100)
+    user = models.ForeignKey(User, related_name='audits')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = AuditMgr()
--- apps/infohub/views.py
+++ apps/infohub/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect
 from django.core.urlresolvers import reverse
-#from .models import InfoSource
+from .models import InfoSource, Audit
 from ..login_reg.models import User
 from django.contrib import messages
+import time
 
 def index(request):
     if "userID" not in request.session:
@@ -15,3 +16,90 @@ def index(request):
         "first_name" : User.objects.get(id = userID).first_name
     }
     return render(request, 'infohub/index.html', context)
+
+
+########## Tests ##########
+
+def runTests(request):
+    user_id = request.session["userID"]
+    test_results = []
+    data = {
+        "source_type": "api",
+        "location" : "Bing",
+        "highlight_text" : "Trump"
+    }
+
+    # Add a source
+    time_start = time.time()
+    result = InfoSource.objects.add(data, user_id)
+    seconds = int(time.time() - time_start)
+    if result.source_type == "api" and result.location == "Bing" and result.highlight_text == "Trump":
+        status = "passed"
+    else:
+        status = "failed"
+
+    test_results.append({
+        "name" : "AddSource",
+        "status" : status,
+        "run_time" : str(seconds)
+    })
+
+    # Update a source
+    data = {
+        "source_type": "url",
+        "location" : "www.msnbc.com/news",
+        "highlight_text" : "Hillary",
+        "source_id" : result.id
+    }
+    time_start = time.time()
+    result = InfoSource.objects.update(data, user_id)
+    seconds = int(time.time() - time_start)
+    if result.source_type == "url" and result.location == "www.msnbc.com/news" and result.highlight_text == "Hillary":
+        status = "passed"
+    else:
+        status = "failed"
+
+    test_results.append({
+        "name" : "UpdateSource",
+        "status" : status,
+        "run_time" : str(seconds)
+    })
+
+    # Get all sources for logged in user.
+    time_start = time.time()
+    result = InfoSource.objects.getActive(user_id)
+    seconds = int(time.time() - time_start)
+    if len(result) > 0:
+        status = "passed"
+    else:
+        status = "failed"
+
+    test_results.append({
+        "name" : "InfoSource:getActive",
+        "status" : status,
+        "run_time" : str(seconds)
+    })
+
+    # Remove a source
+    time_start = time.time()
+    InfoSource.objects.remove(data, user_id)
+    seconds = int(time.time() - time_start)
+    if len(InfoSource.objects.filter(id = data["source_id"])) == 0:
+        status = "passed"
+    else:
+        status = "failed"
+
+    test_results.append({
+        "name" : "RemoveSource",
+        "status" : status,
+        "run_time" : str(seconds)
+    })
+
+    # Get audit trail
+    Audit.objects.audit(user_id, "Ran tests")
+    MAX_AUDIT_ROWS = 20
+    context = {
+        "test_results" : test_results,
+        "audits" : Audit.objects.getAll(MAX_AUDIT_ROWS)
+    }
+    return render(request, 'infohub/runtests.html', context)
